b05423e9301d83d33dffdaa97c1c69b5706865e3
ravilavanya362-creator <ravilavanya362@gmail.com>
2026-08-30 20:40:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "*.hdslb.com" }
+    ]
+  }
+};
+
+module.exports = nextConfig;
 

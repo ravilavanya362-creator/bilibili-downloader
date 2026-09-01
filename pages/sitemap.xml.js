d6f4f8582eac76dac3836284b365f8bf235abc28
ravilavanya362-creator <ravilavanya362@gmail.com>
2026-09-01 15:11:38 +0530

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,51 +1,57 @@
-import { getAllPosts } from "../lib/posts";
-
-const SITE_URL = "https://bilibili-downloader-one.vercel.app";
-
-const STATIC_PATHS = [
-  { path: "/", priority: "1.0" },
-  { path: "/blog", priority: "0.8" },
-  { path: "/about", priority: "0.5" },
-  { path: "/contact", priority: "0.4" },
-  { path: "/privacy", priority: "0.3" },
-  { path: "/terms", priority: "0.3" },
-  { path: "/disclaimer", priority: "0.3" },
-  { path: "/copyright", priority: "0.3" },
-];
-
-function generateSiteMap(posts) {
-  const urls = [
-    ...STATIC_PATHS.map(
-      ({ path, priority }) => `
-  <url>
-    <loc>${SITE_URL}${path}</loc>
-    <priority>${priority}</priority>
-  </url>`
-    ),
-    ...posts.map(
-      (post) => `
-  <url>
-    <loc>${SITE_URL}/blog/${post.slug}</loc>
-    <lastmod>${new Date(post.date).toISOString()}</lastmod>
-    <priority>0.6</priority>
-  </url>`
-    ),
-  ].join("");
-
-  return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}
-</urlset>`;
-}
+import { getAllPosts } from '../lib/posts';
 
 export default function SiteMap() {
-  return null;
+  // getServerSideProps handles the XML generation
 }
 
 export async function getServerSideProps({ res }) {
-  const sitemap = generateSiteMap(getAllPosts());
-  res.setHeader("Content-Type", "text/xml");
+  const baseUrl = 'https://bilisave.com';
+  const posts = getAllPosts();
+
+  const staticPages = [
+    '',
+    '/about',
+    '/blog',
+    '/contact',
+    '/copyright',
+    '/disclaimer',
+    '/privacy',
+    '/terms',
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${staticPages
+    .map((slug) => {
+      return `
+    <url>
+      <loc>${baseUrl}${slug}</loc>
+      <changefreq>daily</changefreq>
+      <priority>${slug === '' ? '1.0' : '0.8'}</priority>
+    </url>
+  `;
+    })
+    .join('')}
+  ${posts
+    .map((post) => {
+      return `
+    <url>
+      <loc>${baseUrl}/blog/${post.slug}</loc>
+      <lastmod>${post.date ? new Date(post.date).toISOString() : new Date().toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.7</priority>
+    </url>
+  `;
+    })
+    .join('')}
+</urlset>
+`;
+
+  res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
   res.end();
-  return { props: {} };
-}
 
+  return {
+    props: {},
+  };
+}
